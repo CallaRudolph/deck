890839a6839a6108ca12e3275cf8d9f4d61f6767
Calla Rudolph <callarudolph@gmail.com>
2017-06-05 21:14:23 -0700

    create var deck with function

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+  // $("#deck").submit(function() {
+  var nums = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
+
+  var suits = ["spades", "clubs", "hearts", "diamonds"];
+
+  nums.forEach(function(num) {
+    suits.forEach(function(suit) {
+      var deck = $("<li>" + num + " of " + suit + "</li>")
+      $("ul#deck").prepend(deck);
+      });
+    });
+});
